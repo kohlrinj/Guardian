07995daa978e2f5de61661cf57a79d1d5c22d4f5
kohlrinj <kohlrinj@mail.uc.edu>
2017-09-05 17:15:58 -0700

    My First Script Windows

---
+++ sysinfo.ps1
@@ -0,0 +1,9 @@
+﻿#Get-NetIPAddress | Where-Object {$_.IPv4Address -like '10*'}
+
+$EmailTo = "kohlrinj@ucmail.uc.edu"
+$EmailFrom = "kohlrinj@ucmail.uc.edu"
+$IP = Get-NetIPAddress | Where-Object {$_.IPv4Address -like '10*'}
+$Body = "User: {0} Hostname: {1} IP: {2}" -f $env:UserName,$env:ComputerName,$IP
+$Subject = "Your Windows IP Address"
+
+Send-MailMessage -SmtpServer smtp.uc.edu -From $EmailFrom -To $EmailTo -Subject $Subject -Body $Body

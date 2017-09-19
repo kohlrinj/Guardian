b2bb521b6f9d1678db9f215a2600bb1b5d169204
kohlrinj <kohlrinj@mail.uc.edu>
2017-09-19 15:12:09 -0700

    Fixed syntax errors

--- numberguesser.py
+++ numberguesser.py
@@ -8,12 +8,12 @@ randInt = random.randint(1,high)
 print("Awesome! Guess a number between 1 and " + str(high) + ".")
 guess = int(input())
 
-while guess != randInt
-    if guess < randInt
+while guess != randInt:
+    if guess < randInt:
         print("Your number is too low! Guess again!")
         guess = int(input())
-    elif guess > randInt
+    elif guess > randInt:
         print("Your number is too high! Guess again!")
         guess = int(input())
 
-print("Awesome! You got it right! The correct number was " + str(randInt) ".")
+print("Awesome! You got it right! The correct number was " + str(randInt))
